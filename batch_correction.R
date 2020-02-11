8baa5fff3b03b5159be897970b0e64da4a5f8c2a
xggzj <55190731+xggzj@users.noreply.github.com>
2020-02-11 23:17:53 +0100

    Add SAUCIE and preprocessing steps
    --Remove outliers
    --Batch correction
    --Scaling

--- batch_correction.R
+++ batch_correction.R
@@ -1,9 +1,10 @@
 # Read infiles
 library(flowCore)
-file_1 = read.FCS("000027_d_C4.fcs")
-file_2 = read.FCS("VG32_(tv.1)-PTc_d_C1.fcs")
+file_1 = read.FCS("000027_d_C4.fcs") # Batch 1 raw data
+file_2 = read.FCS("VG32_(tv.1)-PTc_d_C1.fcs") # Batch 2 raw data
 
-# A function to read the short names from a flowFrame
+# Preprocessing:
+# Read and filter the marker names (short_name) from a flowFrame, remove vacant channels.
 getsnames = function(file){
   batch = file@exprs
   colnames(batch) = file@parameters@data$desc
@@ -17,31 +18,58 @@ getsnames = function(file){
   return(batch)
 }
 
-batch_1 = getsnames(file_1)
+batch_1 = getsnames(file_1) 
 batch_2 = getsnames(file_2)
 
-# Concatenate two batches with cells in the rows and features in the columns
-dat = rbind(batch_1, batch_2)
-# Batch covariate
-batch = as.factor(c(rep(1, nrow(batch_1)), rep(2, nrow(batch_2))))
+# Remove outliers
+removeOutliers = function(Dat){
+  q1 = quantile(Dat, 0.001)
+  q99 = quantile(Dat, 0.999)
+  Dat[Dat<q1] = q1
+  Dat[Dat>q99] = q99
+  return(Dat)
+}
+
+batch_1 = apply(batch_1, 2, removeOutliers)
+batch_2 = apply(batch_2, 2, removeOutliers)
 
 # 1. Batch correction:
+# For SAUCIE: 
+# Save the pre-processed matrix of the two batches as .fcs files to run SAUCIE correction in the terminal
+flowframe_1 = flowFrame(batch_1)
+write.FCS(flowframe_1, "000027_d_C4_processed.fcs")
+flowframe_2 = flowFrame(batch_2)
+write.FCS(flowframe_1, "VG32_(tv.1)-PTc_d_C1_processed.fcs")
+
+# Read infiles after SAUCIE-batch-corrected
+file_3 = read.FCS("000027_d_C4_saucie.fcs") # Batch 1 SAUCIE-corrected data
+file_4 = read.FCS("VG32_(tv.1)-PTc_d_C1_saucie.fcs") # Batch 2 SAUCIE-corrected data
+# Concatenate two batch-corrected data into a single matrix 
+dat.saucie = rbind(file_3@exprs, file_4@exprs)
+
+# For ComBat, limma and Harmony:  
+# Concatenate the raw data of two batches into a single matrix with cells in the rows and features in the columns
+dat = rbind(batch_1, batch_2) # Raw data of two batches before batch-correction
+batch = as.factor(c(rep(1, nrow(batch_1)), rep(2, nrow(batch_2)))) # Batch covariate
+
 # The input for ComBat and limma should be a matrix with features in the rows and samples in the columns
 # The input for Harmony should be a matrix with samples in the rows and features in the columns
+# Scaling after correction 
 
 # ComBat
 library(sva)
 dat.combat = ComBat(t(dat), batch)  
 dat.combat = t(dat.combat)
-
+dat.combat = scale(dat.combat)
 # Limma
 library(limma)
 dat.limma = removeBatchEffect(t(dat), batch)
 dat.limma = t(dat.limma)
-
+dat.limma = scale(dat.limma)
 # Harmony
 library(harmony)
 dat.harmony = HarmonyMatrix(dat, batch)
+dat.harmony = scale(dat.harmony)
 
 # 2. Visulization:
 library(ggplot2)
@@ -70,6 +98,8 @@ umap.combat = Umap(dat.combat, batch, "ComBat")
 umap.limma = Umap(dat.limma, batch, "limma")
 # Harmony
 umap.harmony = Umap(dat.harmony, batch, "Harmony")
+# SAUCIE
+umap.saucie = Umap(dat.saucie, batch, "SAUCIE")
 
 # (2) t-SNE
 # Cells in rows, features in columns
@@ -95,6 +125,8 @@ tsne.combat = Tsne(dat.combat, batch, "ComBat")
 tsne.limma = Tsne(dat.limma, batch, "limma")
 # Harmony
 tsne.harmony = Tsne(dat.harmony, batch, "Harmony")
+# SAUCIE
+tsne.saucie = Tsne(dat.saucie, batch, "SAUCIE")
 
 # 3. Metrics:
 # PCA and select the top 20 PCs
@@ -112,11 +144,14 @@ pc20.combat = PC20(t(dat.combat))
 pc20.limma = PC20(t(dat.limma))
 # Harmony
 pc20.harmony = PC20(t(dat.harmony))
+# SAUCIE
+pc20.saucie = PC20(t(dat.saucie))
 
 # (1) kBET
 library(kBET)
 KBET = function(pc20, Batch){
-  # k input value equal to 5%, 10%, 15%, 20%, and 25% of the sample size
+  # Run kBET using a predefined list of k values (equal to 5%, 10%, 15%, 20%, and 25% of the sample size)
+  # Get the median of all kBET rejection rates of each input of k value
   kBET.5 = kBET(pc20, Batch, k0 = 0.05*nrow(pc20), plot = FALSE)
   median.5 = median(kBET.5$stats$kBET.observed)
   kBET.10 = kBET(pc20, Batch, k0 = 0.1*nrow(pc20), plot = FALSE)
@@ -127,6 +162,7 @@ KBET = function(pc20, Batch){
   median.20 = median(kBET.20$stats$kBET.observed)
   kBET.25 = kBET(pc20, Batch, k0 = 0.25*nrow(pc20), plot = FALSE)
   median.25 = median(kBET.25$stats$kBET.observed)
+  # Calculate the median of the five different k values as the final kBET score
   median = median(c(median.5, median.10, median.15, median.20, median.25))
   result = list("5%" = median.5, "10%" = median.10, "15%" = median.15, "20%" = median.20, "25%" = median.25, "median" = median)
   return(result)
@@ -138,11 +174,15 @@ kBET.combat = KBET(pc20.combat, batch)
 kBET.limma = KBET(pc20.limma, batch)
 # Harmony
 kBET.harmony = KBET(pc20.harmony, batch)
+# SAUCIE
+kBET.saucie = KBET(pc20.saucie, batch)
 
 # (2) LISI
 library(lisi)
+# iLISI: integration LISI
 LISI = function(pc20, Batch){
   dat.lisi = compute_lisi(pc20, data.frame(batch = Batch), 'batch')
+  # Use the maximum and minimum score to scale the lisi scores
   dat.lisi.scale = (dat.lisi$batch-min(dat.lisi$batch))/(max(dat.lisi$batch)-min(dat.lisi$batch)) 
   median = median(dat.lisi.scale)
   return(median)
@@ -154,20 +194,23 @@ lisi.combat = LISI(pc20.combat, batch)
 lisi.limma = LISI(pc20.limma, batch)
 # Harmony
 lisi.harmony = LISI(pc20.harmony, batch)
+# SAUCIE
+lisi.saucie = LISI(pc20.saucie, batch)
 
 # (3) ASW
 library(fpc)
 ASW20 = function(pc20){
-  # determine optimal clusters
+  # Determine the optimal clusters
   pamk.best = pamk(pc20)
   asw20 = c()
-  # Repeat ASW for 20 times and calculate the median of 20 ASW scores
+  # To get a stable result, repeat ASW for 20 times and calculate the median of 20 ASW scores
   for (i in 1:20){
     pamk.result = kmeans(pc20, pamk.best$nc)
     pamk.stats = cluster.stats(dist(pc20)^2, pamk.result$cluster)
     asw = pamk.stats$avg.silwidth
     asw20[i] = asw
   }
+  # Normalize the ASW scores to 0-1
   asw20_scale = (asw20-min(asw20))/(max(asw20)-min(asw20)) 
   asw_median = median(asw20)
   return(asw_median)
@@ -179,16 +222,20 @@ ASW.combat = ASW20(pc20.combat)
 ASW.limma = ASW20(pc20.limma)
 # Harmony
 ASW.harmony = ASW20(pc20.harmony)
+# SAUCIE
+ASW.saucie = ASW20(pc20.saucie)
 
 # (4) ARI
 library(stats)
 library(mclust)
 ARI20 = function(pc20, Batch){
   ari20 = c()
+  # To get a stable result, repeat ARI for 20 times
   for (i in 1:20){
     dat.kmeans = kmeans(pc20, 2)
     ari20[i] = adjustedRandIndex(Batch, dat.kmeans$cluster)
   }
+  # Normalize the ASW scores to 0-1
   ari20_scale = (ari20-min(ari20))/(max(ari20)-min(ari20))
   ari_median = median(ari20_scale)
   return(ari_median)
@@ -200,5 +247,5 @@ ARI.combat = ARI20(pc20.combat, batch)
 ARI.limma = ARI20(pc20.limma, batch)
 # Harmony
 ARI.harmony = ARI20(pc20.harmony, batch)
-
-
+# SAUCIE
+ARI.saucie = ARI20(pc20.saucie, batch)
