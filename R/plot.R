bf3073d771372a072198d5d505d919c664625ad9
xggzj <qiche@kth.se>
2020-02-26 15:10:41 +0100

    Update plot.R

--- R/plot.R
+++ R/plot.R
@@ -1,21 +1,12 @@
 library(ggplot2)
 
 umapPlot = function(umap, method, colored_by = c("batch", "level1", "level2")){
-  if(colored_by == "batch"){
-    df = data.frame(UMAP1 = umap$layout[,1],
-                    UMAP2 = umap$layout[,2],
-                    Label = Label$batch)
-  }
-  if(colored_by == "level1"){
-    df = data.frame(UMAP1 = umap$layout[,1],
-                    UMAP2 = umap$layout[,2],
-                    Label = Label$level1)
-  }
-  if(colored_by == "level2"){
-    df = data.frame(UMAP1 = umap$layout[,1],
-                    UMAP2 = umap$layout[,2],
-                    Label = Label$level2)
-  }
+  if(colored_by == "batch"){Label = MetaData$batch}
+  if(colored_by == "level1"){Label = MetaData$level1}
+  if(colored_by == "level2"){Label = MetaData$level2}
+  df = data.frame(UMAP1 = umap$layout[,1],
+                  UMAP2 = umap$layout[,2],
+                  Label = Label)
   gp = ggplot(df, aes(UMAP1, UMAP2, color = Label)) +
     geom_point(size = -0.1, alpha = 0.1) +
     ggtitle(method) +
